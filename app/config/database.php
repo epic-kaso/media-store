902d8a2e348d6b2b85306f48d5b9d5b21e305a30
Akachukwu Okafor <kasoprecede47@gmail.com>
2014-09-24 13:15:34 +0100

    bug fix

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+            'host'     => 'localhost',
+            'database' => 'forge',
+            'username' => 'forge',
+            'password' => 'ZngIVW1kSmTOfriCt1k3',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
